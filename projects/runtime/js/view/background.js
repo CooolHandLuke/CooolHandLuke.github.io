4567f48c5b92769e334bd572de3d4a1d6b4e39e7
Luke Johnson <benjaminlukejohnson@gmail.com>
2023-04-21 16:01:10 +0000

    circularity

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -25,29 +25,69 @@ var background = function (window) {
         // container which will be returned
         var background;
         
-        // ANIMATION VARIABLES HERE:
-        
-     
+        //////////////////////////////////////////////////////////////////
+        // ANIMATION VARIABLES HERE //////////////////////////////////////
+        //////////////////////////////////////////////////////////////////
+        // TODO (several):
+        var tree;
+        var lightpost;
+        var buildings = [];
+      
+      
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
             background.removeAllChildren();
 
-            // TODO: 2 - Part 2
-            // this fills the background with a obnoxious yellow
-            // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            // TODO 1:
+            // this currently fills the background with an obnoxious yellow;
+            // you should modify both the height and color to suit your game
+            var backgroundFill = draw.rect(canvasWidth, canvasHeight, 'purple');
             background.addChild(backgroundFill);
+        
+            // TODO 2: - Add a moon and starfield
+            var moon = draw.bitmap("img/moon.png");
+            moon.x = 600;
+            moon.y = 50;
+            moon.scaleX = .6;
+            moon.scaleY = .6;
+            background.addChild(moon);
             
-            // TODO: 3 - Add a moon and starfield
+            for (var i = 0; i < 100; i++) {
+            var circle = draw.circle(10, "white", "LightGray", 2);
+            circle.x = canvasWidth * Math.random();
+            circle.y = groundY * Math.random();
+            background.addChild(circle);
+            }
+
             
             
-            // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
+            // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
+            for (var i = 0; i < 5; ++i) {
+                var buildingHeight = groundY * Math.random();
+                var building = draw.rect(75, buildingHeight, "LightGray", "Black", 1);
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+              }
             
+            // TODO 3: Part 1 - Add a tree
             
-            // TODO 4: Part 1 - Add a tree
+            tree = draw.bitmap("img/tree.png");
+            tree.x = 0;
             
+            tree.y = groundY - 200;
+            background.addChild(tree);
             
+            lightpost = draw.bitmap("img/street-light.png");
+            lightpost.x = 0;
+            
+            lightpost.y = groundY - 200;
+            lightpost.scaleX = .08
+            lightpost.scaleY = .08
+            background.addChild(lightpost);
+
         } // end of render function - DO NOT DELETE
         
         
@@ -56,13 +96,26 @@ var background = function (window) {
         function update() {
             // useful variables
             var canvasWidth = app.canvas.width;
-            var canvasHeight = app.canvas.height;
+            var canvasHeight =  app.canvas.height;
             var groundY = ground.y;
             
-            // TODO 4: Part 2 - Move the tree!
+            // TODO 3: Part 2 - Move the tree!
+            tree.x = tree.x - 1;
+
+            if (tree.x < -200) {
+            tree.x = canvasWidth;
+            }
             
+            // TODO 4: Part 2 - Parallax
             
-            // TODO 5: Part 2 - Parallax
+            for (var i = 0; i < buildings.length; i++) {
+                var building = buildings[i];
+                building.x = building.x - .2
+              if (building.x < -200) {
+                building.x = canvasWidth
+              }
+                // code to do something with each element
+              }
             
 
         } // end of update function - DO NOT DELETE
